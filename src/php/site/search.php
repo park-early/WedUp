0ba3554e140f921aad87bbec73f5e2959476eea2
Drea <asyquia@students.cs.ubc.ca>
2023-03-29 23:39:04 -0700

    create bare view companies & rates, view weddings, and search & filter pages

---
+++ src/php/site/search.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>WedUP</title>
+    </head>
+    <body>
+        <h1 style="text-align:center">Search</h1>
+        <form action="search.php" style="text-align:center">
+        <input type="search" id="search-weddings" name="search-weddings">
+        <input type="submit" value="Submit" name="insertSubmit"></p>
+        </form>
+    </body>
+</html>
