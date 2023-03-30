0c8f26bd1855fb7dfdb6c491a9a34b14a8b734ac
Drea <asyquia@students.cs.ubc.ca>
2023-03-29 22:54:33 -0700

    create add staff page

---
+++ src/php/site/staff.php
@@ -0,0 +1,32 @@
+<html>
+    <head>
+        <title>WedUP</title>
+    </head>
+    <body>
+        <form action="staff.php">
+            <h3>Wedding information</h3>
+            <label for="wedding">Select wedding to add staff to:</label>
+            <select id="wedding" name="wedding" required>
+                <option value="Wedding1">Wedding 1</option>
+                <option value="Wedding2">Wedding 2</option>
+                <option value="Wedding3">Wedding 3</option>
+            </select>
+            <hr>
+            <h3>Staff information</h3>
+            <label for="stafffname">First name:</label>
+            <input type="text" id="stafffname" name="stafffname" required>
+            <label for="stafflname">Last name:</label>
+            <input type="text" id="stafflname" name="stafflname" required><br>
+            <label for="staffemail">Email:</label>
+            <input type="email" id="staffemail" name="staffemail" required>
+            <label for="staffcompany">Company:</label>
+            <select id="company" name="company" required>
+                <option value="Company1">Company 1</option>
+                <option value="Company2">Company 2</option>
+                <option value="Company3">Company 3</option>
+            </select>
+            <hr>
+            <input type="submit" value="Submit" name="insertSubmit"></p>
+        </form>
+    </body>
+</html>
